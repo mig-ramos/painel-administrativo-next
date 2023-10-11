2820ce11e26311a7086db5f5d61d2b18e260d645
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-10-11 00:21:33 -0300

    feat: cria template da aplicacao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import Layout from "../components/template/Layout";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Admin</h1>
-    </main>
+    <Layout titulo="Página Inicial" subtitulo="Estamos construíndo um template Admin!">
+    <h3>Conteúdo!!!!</h3>
+   </Layout>
   );
 }
