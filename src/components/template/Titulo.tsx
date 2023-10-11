a72ef880d0355b3de164cf48587cae8718aec529
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-10-11 20:41:21 -0300

    Estilizando o cabecalho

--- src/components/template/Titulo.tsx
+++ src/components/template/Titulo.tsx
@@ -8,12 +8,14 @@ interface TituloProps {
       <div>
         <h1
           className={`
+          font-black text-3xl
+          text-gray-900
           `}>
               {props.titulo}
         </h1>
         <h2
           className={`
-                  
+             font-light text-sm text-gray-600  
           `}>
               {props.subtitulo}
           </h2>
