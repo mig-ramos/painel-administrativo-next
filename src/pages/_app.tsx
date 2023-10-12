aa1dfa16bc18051c46c09771cff7763cfa876310
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-10-12 17:09:22 -0300

    adiciona imagem e mensagem de erro na tela de login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 import type { AppProps } from "next/app";
 import { AppProvider } from "../data/context/AppContext";
+import { ToastContainer } from "react-toastify";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppProvider>
       <Component {...pageProps} />
+      <ToastContainer autoClose={2000} />
     </AppProvider>
   );
 }
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,56 +1,92 @@
 import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
+import Image from "next/image";
+import figura from "public/background.png";
+import imgLoading from "public/loading.gif";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function Auth() {
-  const [modo, setModo] = useState<'login' | 'cadastro'>('login')
+  const [modo, setModo] = useState<"login" | "cadastro">("login");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  function submeter() {
-    if(modo === 'login') {
-      console.log('login')
+  function clearForm() {
+    setEmail("");
+    setPassword("");
+  }
+
+  async function submeter() {
+    if (modo === "login") {
+      if (email === "" || password === "") {
+        toast.warning("PREENCHA OS DADOS...");
+        return;
+      }
+      setLoading(true);
+      console.log("login");
+      clearForm();
+      setLoading(false);
     } else {
-      console.log('cadastrar')
+      console.log("cadastrar");
     }
   }
 
   return (
-    <div className={`flex flex-col h-sreen items-center 
-    justify-center
-    `}>
+    <div className="flex h-screen justify-center items-center">
+      <div className={`hidden md:block md:w-1/2 lg:w-2/3`}>
+        <Image
+          src={figura}
+          alt="Image da tela de autenticação"
+          priority={true}
+          style={{
+            width: "auto",
+            height: "100vh",
+            objectFit: 'cover'
+          }}
+        />
+      </div>
 
-   
-    <div className={`w-1/2`}>
-      <h1 className={`text-xl font-bold mb-5`}>{modo === 'login' ? 'Entre com a sua conta' : 'Cadastre-se na plataforma'}</h1>
-      <AuthInput
-        label="Email"
-        tipo="email"
-        valor={email}
-        valorMudou={setEmail}
-        obrigatorio
-      />
-      <AuthInput
-        label="Senha"
-        tipo="password"
-        valor={password}
-        valorMudou={setPassword}
-        obrigatorio
-      />
-      <AuthInput
-        label="Confirmação de Senha"
-        tipo="password"
-        valor={password}
-        valorMudou={setPassword}
-        obrigatorio
-        naoRenderizarQuando={true}
-      />
-      <button onClick={submeter} className={`
+      <div className={`m-10 w-full md:w-1/2 lg:w-1/3`}>
+        <h1 className={`text-3xl font-bold mb-5`}>
+          {modo === "login"
+            ? "Entre com a sua conta"
+            : "Cadastre-se na plataforma"}
+        </h1>
+        <AuthInput
+          label="Email"
+          tipo="email"
+          valor={email}
+          valorMudou={setEmail}
+          obrigatorio
+        />
+        <AuthInput
+          label="Senha"
+          tipo="password"
+          valor={password}
+          valorMudou={setPassword}
+          obrigatorio
+        />
+        <AuthInput
+          label="Confirmação de Senha"
+          tipo="password"
+          valor={password}
+          valorMudou={setPassword}
+          obrigatorio
+          naoRenderizarQuando={true}
+        />
+        <button
+          onClick={submeter}
+          disabled={loading}
+          className={`
+          flex justify-center
       w-full bg-indigo-500 hover:bg-indigo-400
       text-white rounded-lg px-4 py-3 mt-6
-      `}>
-        {modo === 'login' ? 'Entrar' : 'Cadastrar'}
-      </button>
-    </div>
+      `}
+        >
+          <Image src={imgLoading} alt="Loading" hidden={!loading} width={24} className="mr-2" />
+          {modo === "login" ? "Entrar" : "Cadastrar"}
+        </button>
+      </div>
     </div>
   );
 }
