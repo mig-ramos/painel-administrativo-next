169da175c2ebe9a5004ebd6869ac40762d5a8b0c
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-10-11 21:18:10 -0300

    inseri logo no menu lateral

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,14 +1,23 @@
-import { IconeAjustes, IconeCasa, IconeSino } from "../icons";
+import { IconeLogo, IconeAjustes, IconeCasa, IconeSino } from "../icons";
 import MenuItem from "./MenuItem";
 
-export default function MenuLateral(){
-    return(
-        <aside>
-           <ul>
-            <MenuItem url="/" texto="Home" icone={IconeCasa} />
-            <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
-            <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
-           </ul>
-        </aside>
-    )
+export default function MenuLateral() {
+  return (
+    <aside>
+      <div
+        className={`
+                flex flex-col items-center justify-center
+                bg-gradient-to-r from-indigo-200 to-purple-400
+                h-20 w-20 p-2
+            `}
+      >
+        {IconeLogo}
+      </div>
+      <ul>
+        <MenuItem url="/" texto="Home" icone={IconeCasa} />
+        <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
+        <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+      </ul>
+    </aside>
+  );
 }
