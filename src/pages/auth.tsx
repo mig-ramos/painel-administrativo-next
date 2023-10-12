d325a4859b326751b1a047762944f7f9c973d3f0
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-10-12 12:05:04 -0300

    aplicat estilo aos inputs e tela de autenticacao

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -6,9 +6,22 @@ export default function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  function submeter() {
+    if(modo === 'login') {
+      console.log('login')
+    } else {
+      console.log('cadastrar')
+    }
+  }
+
   return (
-    <div>
-      <h1>Autenticação</h1>
+    <div className={`flex flex-col h-sreen items-center 
+    justify-center
+    `}>
+
+   
+    <div className={`w-1/2`}>
+      <h1 className={`text-xl font-bold mb-5`}>{modo === 'login' ? 'Entre com a sua conta' : 'Cadastre-se na plataforma'}</h1>
       <AuthInput
         label="Email"
         tipo="email"
@@ -31,6 +44,13 @@ export default function Auth() {
         obrigatorio
         naoRenderizarQuando={true}
       />
+      <button onClick={submeter} className={`
+      w-full bg-indigo-500 hover:bg-indigo-400
+      text-white rounded-lg px-4 py-3 mt-6
+      `}>
+        {modo === 'login' ? 'Entrar' : 'Cadastrar'}
+      </button>
+    </div>
     </div>
   );
 }
